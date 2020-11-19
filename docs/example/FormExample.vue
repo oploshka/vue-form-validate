53926fed67c55979f32a0bf4f3ca739aa25d3057
Andrey Tyurin <ectb08@mail.ru>
2020-11-20 01:15:14 +0300

    add doc and example

---
+++ docs/example/FormExample.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="formSubmit" novalidate="novalidate">
+    <input type="submit" value="submit" style="display: none !important;">
+    <!-- form start -->
+
+    <TextFormElement
+      v-model="form.name"
+    />
+
+    <!-- form end -->
+  </form>
+</template>
+
+<script>
+
+import TextFormElement from "@FormValidate/Element/FveText";
+
+import FveFormMixin   from "@FormValidate/FveFormMixin";
+
+export default {
+  mixins: [
+    FveFormMixin
+  ],
+  components: {
+    TextFormElement,
+  },
+  methods: {
+    formSchema() {
+      return {
+        name: {
+          type: String,
+          default: () => { return ''; }
+        },
+      };
+    },
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+</style>
